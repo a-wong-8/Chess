e05ddd0f79bbf40296c0ffeb91140d75af740f19
a-wong-8 <awong88@berkeley.edu>
2023-04-18 14:42:12 -0700

    v2

--- chess/board.rb
+++ chess/board.rb
@@ -1,51 +1,55 @@
 require_relative "piece.rb"
 class Board
     
-
     def initialize
         @board = Array.new(8) {Array.new(8)}
-        fill_board(@board)
-        print(@board)
+       fill_board(@board)
+        # print(@board)
     end
+
     def [](position)
         row, col = position
         @board[row][col]
     end
+
     def []=(position, value)
         row, col = position
         @board[row][col] = value 
     end
+
     def fill_board(board)
-        black_rook = Piece.new(:rook, :black)
-        board[[0,0]] = black_rook.type_piece
-        board[0,1] = Piece.new(:knight, :black) 
-        board[0,2] = Piece.new(:bishop, :black)
-        board[0,3] = Piece.new(:queen, :black)
-        board[0,4] = Piece.new(:king, :black) 
-        board[0,5] = Piece.new(:bishop, :black)
-        board[0,6] = Piece.new(:knight, :black)
-        board[0,7] = Piece.new(:rook, :black)
-        board[7,0] = Piece.new(:rook, :white)
-        board[7,1] = Piece.new(:knight, :white)
-        board[7,2] = Piece.new(:bishop, :white)
-        board[7,3] = Piece.new(:queen, :white) 
-        board[7,4] = Piece.new(:king, :white)
-        board[7,5] = Piece.new(:bishop, :white)
-        board[7,6] = Piece.new(:knight, :white)
-        board[7,7] = Piece.new(:rook, :white)
+        self[[0,0]] = Piece.new(:rook, :black)
+        self[[0,1]] = Piece.new(:knight, :black) 
+        self[[0,2]] = Piece.new(:bishop, :black)
+        self[[0,3]] = Piece.new(:queen, :black)
+        self[[0,4]] = Piece.new(:king, :black) 
+        self[[0,5]] = Piece.new(:bishop, :black)
+        self[[0,6]] = Piece.new(:knight, :black)
+        self[[0,7]] = Piece.new(:rook, :black)
+        self[[7,0]] = Piece.new(:rook, :white)
+        self[[7,1]] = Piece.new(:knight, :white)
+        self[[7,2]] = Piece.new(:bishop, :white)
+        self[[7,3]] = Piece.new(:queen, :white) 
+        self[[7,4]] = Piece.new(:king, :white)
+        self[[7,5]] = Piece.new(:bishop, :white)
+        self[[7,6]] = Piece.new(:knight, :white)
+        self[[7,7]] = Piece.new(:rook, :white)
         (0..7).each do |x|
-            board[1,x] = Piece.new(:pawn, :black)
-            board[6,x] = Piece.new(:pawn, :white)
+            self[[1,x]] = Piece.new(:pawn, :black)
+            self[[6,x]] = Piece.new(:pawn, :white)
         end
         (2..5).each do |x|
             (0..7).each do |y|
-              board[x,y] = Piece.new(:null_piece, :null)
+                self[[x,y]] = Piece.new(:null_piece, :null)
             end
         end
     end
-    def print(board)
-        board.each do |row|
+
+    def print_board
+        @board.each do |row|
             puts row.join(" ")
         end
+        puts 'board has been printed'
     end
+
 end
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,6 +1,6 @@
 
 class Piece 
-    attr_reader :color :type_piece
+    attr_reader :color, :type_piece
     def initialize(type_piece, color)
         @color = color 
         @type_piece = type_piece
